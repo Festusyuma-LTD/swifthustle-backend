0084c74e6f2443c838e35c152e832dde80ab381e
Ridwan <ridwan.mustapha@longbridgetech.com>
2019-12-05 10:08:36 +0100

    GameFullService

---
+++ app/Service/GameFullService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+//use App\Repository\ValidGameRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class GameFullService {
+
+    protected $validGameRepository;
+
+    public function __construct(ValidGameRepository $validGameRepository)
+    {
+        $this->validGameRepository = $validGameRepository;
+    }
+
+    public function makeGame(Request $request) {
+
+        $validGame = Validator::make($request->all(), [
+           'amount' => 'required',
+            'odd' => 'required',
+            'active' => 'required'
+        ]);
+
+        if($validGame->fails()) {
+            return response()->json(['status' => 430, 'message' => 'operation failed', 'data' => $validGame->messages()->all()]);
+        } else {
+            $game = $this->validGameRepository->add($request);
+            if ($game) {
+                return response()->json(['status' => 200, 'message' => 'operation successful', 'data' => $game]);
+            } else {
+                return response()->json(['status' => 400, 'message' => 'operation failed']);
+            }
+        }
+    }
+}
